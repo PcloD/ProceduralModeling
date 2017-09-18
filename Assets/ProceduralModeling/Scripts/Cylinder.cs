1a455069815e58491ba72191e984cb56ab0df177
mattatz <supa.matz.naka@gmail.com>
2017-09-18 14:35:47 +0900

    Add Tubular from unity-tubular. Update Cylinder.

--- Assets/ProceduralModeling/Scripts/Cylinder.cs
+++ Assets/ProceduralModeling/Scripts/Cylinder.cs
@@ -10,41 +10,29 @@ namespace ProceduralModeling {
 		[SerializeField, Range(3, 32)] protected int segments = 16;
 		[SerializeField] bool openEnded = true;
 
+		// 円を形作るための2π
+		const float PI2 = Mathf.PI * 2f;
+
 		protected override Mesh Build() {
 			var mesh = new Mesh();
 
-			var hh = height * 0.5f;
 			var vertices = new List<Vector3>();
+			var normals = new List<Vector3>();
 			var uvs = new List<Vector2>();
 			var triangles = new List<int>();
+
+			// 上端の高さと、下端の高さ
+			float top = height * 0.5f, bottom = -height * 0.5f;
+
+			// 側面を構成する頂点データを生成
+			GenerateCap(segments, top, bottom, radius, vertices, uvs, normals, true);
 
 			// 側面の三角形を構築する際、円上の頂点を参照するために、
 			// indexが円を一周するための除数
 			var len = segments * 2;
 
-			// 円を形作るための2π
-			var pi2 = Mathf.PI * 2f;
-
+			// 上端と下端をつなぎ合わせて側面を構築
 			for (int i = 0; i < segments; i++) {
-				// 0.0 ~ 1.0
-				float ratio = (float)i / (segments - 1);
-
-				// 0.0 ~ 2π
-				float rad = ratio * pi2;
-
-				// 円に沿って上端と下端に均等に頂点を配置する
-				var top = new Vector3(Mathf.Cos(rad) * radius, hh, Mathf.Sin(rad) * radius);
-				var bottom = new Vector3(Mathf.Cos(rad) * radius, - hh, Mathf.Sin(rad) * radius);
-
-				// 上端
-				vertices.Add(top); 
-				uvs.Add(new Vector2(ratio, 1f));
-
-				// 下端
-				vertices.Add(bottom); 
-				uvs.Add(new Vector2(ratio, 0f));
-
-				// 上端と下端をつなぎ合わせて側面を構築
 				int idx = i * 2;
 				int a = idx, b = idx + 1, c = (idx + 2) % len, d = (idx + 3) % len;
 				triangles.Add(a);
@@ -56,40 +44,83 @@ namespace ProceduralModeling {
 				triangles.Add(b);
 			}
 
+			// 上端と下端の蓋を生成
 			if(openEnded) {
-				vertices.Add(new Vector3(0f, hh, 0f)); // top
+				// 蓋のモデルのための頂点は、ライティング時に異なった法線を利用するために、側面とは共有せずに新しく追加する
+				GenerateCap(segments, top, bottom, radius, vertices, uvs, normals, false);
+
+				// 上端の蓋の真ん中の頂点
+				vertices.Add(new Vector3(0f, top, 0f));
 				uvs.Add(new Vector2(0.5f, 1f));
+				normals.Add(new Vector3(0f, 1f, 0f));
 
-				vertices.Add(new Vector3(0f, -hh, 0f)); // bottom
+				// 下端の蓋の真ん中の頂点
+				vertices.Add(new Vector3(0f, bottom, 0f)); // bottom
 				uvs.Add(new Vector2(0.5f, 0f));
+				normals.Add(new Vector3(0f, -1f, 0f));
 
-				var top = vertices.Count - 2;
-				var bottom = vertices.Count - 1;
+				var it = vertices.Count - 2;
+				var ib = vertices.Count - 1;
 
-				int n = segments * 2;
+				// 側面の分の頂点indexを参照しないようにするためのoffset
+				var offset = len;
 
-				// top side
-				for (int i = 0; i < n; i += 2) {
-					triangles.Add(top);
-					triangles.Add((i + 2) % n);
-					triangles.Add(i);
+				// 上端の蓋の面
+				for (int i = 0; i < len; i += 2) {
+					triangles.Add(it);
+					triangles.Add((i + 2) % len + offset);
+					triangles.Add(i + offset);
 				}
 
-				// bottom side
-				for (int i = 1; i < n; i += 2) {
-					triangles.Add(bottom);
-					triangles.Add(i);
-					triangles.Add((i + 2) % n);
+				// 下端の蓋の面
+				for (int i = 1; i < len; i += 2) {
+					triangles.Add(ib);
+					triangles.Add(i + offset);
+					triangles.Add((i + 2) % len + offset);
 				}
 			}
 
 			mesh.vertices = vertices.ToArray();
 			mesh.uv = uvs.ToArray();
+			mesh.normals = normals.ToArray();
 			mesh.triangles = triangles.ToArray();
-			mesh.RecalculateNormals();
 			mesh.RecalculateBounds();
 
 			return mesh;
+		}
+
+		void GenerateCap(int segments, float top, float bottom, float radius, List<Vector3> vertices, List<Vector2> uvs, List<Vector3> normals, bool side) {
+			for (int i = 0; i < segments; i++) {
+				// 0.0 ~ 1.0
+				float ratio = (float)i / (segments - 1);
+
+				// 0.0 ~ 2π
+				float rad = ratio * PI2;
+
+				// 円周に沿って上端と下端に均等に頂点を配置する
+				float cos = Mathf.Cos(rad), sin = Mathf.Sin(rad);
+				float x = cos * radius, z = sin * radius;
+				Vector3 tp = new Vector3(x, top, z), bp = new Vector3(x, bottom, z);
+
+				// 上端
+				vertices.Add(tp); 
+				uvs.Add(new Vector2(ratio, 1f));
+
+				// 下端
+				vertices.Add(bp); 
+				uvs.Add(new Vector2(ratio, 0f));
+
+				if(side) {
+					// 側面の外側を向く法線
+					var normal = new Vector3(cos, 0f, sin);
+					normals.Add(normal);
+					normals.Add(normal);
+				} else {
+					normals.Add(new Vector3(0f, 1f, 0f)); // 蓋の上を向く法線
+					normals.Add(new Vector3(0f, -1f, 0f)); // 蓋の下を向く法線
+				}
+			}
+
 		}
 
 	}
